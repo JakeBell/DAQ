f21aa4777efe90eb1642433ed1a9e2af30991967
Jake Bell <ravennafootball51@yahoo.com>
2012-07-23 15:23:15 -0400

    workin to a result

--- darkDAQ.py
+++ darkDAQ.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+from datetime import datetime
+
 import VISA_data_types as visa
 import get_xml_data as xml
+import struct_test as structs
 import ctypes 
 
 #import the driver for use
@@ -43,20 +46,20 @@ def Configure():
     settings_file = "example_file_name.xml"
     settings_dict = xml.get_settings_list(settings_file)
     
-    sampInterval = visa.ViReal(settings_dict('sampInterval'))
-    delayTime = visa.ViReal(settings_dict('delayTime'))
+    sampInterval = visa.ViReal(float(settings_dict[0]['sampInterval']))
+    delayTime = visa.ViReal(float(settings_dict[0]['delayTime']))
     
-    nbrSamples = visa.ViInt32(settings_dict('nbrSamples'))
-    nbrSegments = visa.ViInt32(settings_dict('nbrSegments'))
+    nbrSamples = visa.ViInt32(int(settings_dict[0]['nbrSamples']))
+    nbrSegments = visa.ViInt32(int(settings_dict[0]['nbrSegments']))
     
-    coupling = visa.ViInt32(settings_dict('coupling'))
-    bandwitdth = visa.ViInt32(settings_dict('bandwidth'))
-    fullScale = visa.ViReal64(settings_dict('fullScale'))
-    offset = visa.ViReal64(settings_dict('offset'))
+    coupling = visa.ViInt32(int(settings_dict[0]['coupling']))
+    bandwidth = visa.ViInt32(int(settings_dict[0]['bandwidth']))
+    fullScale = visa.ViReal64(float(settings_dict[0]['fullScale']))
+    offset = visa.ViReal64(float(settings_dict[0]['offset']))
     
-    trigCoupling = visa.ViInt32(settings_dict('trigCoupling'))
-    trigSlope = visa.ViInt32(settings_dict('trigSlope'))
-    trigLevel = visa.ViReal64(settings_dict('trigLevel'))
+    trigCoupling = visa.ViInt32(int(settings_dict[0]['trigCoupling']))
+    trigSlope = visa.ViInt32(int(settings_dict[0]['trigSlope']))
+    trigLevel = visa.ViReal64(float(settings_dict[0]['trigLevel']))
             
     #Configure timebase
     status = AqDrv4.AcqrsD1_configHorizontal(InstrumentID[0], sampInterval, delayTime)
@@ -77,7 +80,7 @@ def Configure():
     status = AqDrv4.AcqrsD1_configTrigSource(InstrumentID[0], setting1, trigCoupling, trigSlope, trigLevel, setting00)
 
     #Setup for multisegment readout
-    status = AqDrv4.Acqrs_getInstrumentInfo(InstrumentID[0], visa.ViString("TbNextSegmentPad"), ctypes.byref(tbNextSegmentPad)
+    status = AqDrv4.Acqrs_getInstrumentInfo(InstrumentID[0], visa.ViString("TbNextSegmentPad"), ctypes.byref(tbNextSegmentPad))
 
 def Acquire():
     """Start the acquisition of the waveform"""
@@ -90,7 +93,7 @@ def Acquire():
         AqDrv4.AcqrsD1_stopAcquisition(InstrumentID[0])
         print "Acquisition has timed out - Acquisition stopped - Data taken is invalid"
 
-def Readout()
+def ReadOut():
     """Read out the data acquired from the digitizer"""
     
     settings_file = "example_file_name.xml"
@@ -102,11 +105,44 @@ def Readout()
 
     status = AqDrv4.AcqrsD1_getMemory(InstrumentID[0], ctypes.byref(nbrSamples), ctypes.byref(nbrSegments))
     #Still need to figure out the readPar struct datatype to readout
+    
+    readPar = structs.AqReadParameters()
+    dataDesc = structs.AqDataDescriptor()
+    # AqSegmentDescriptor* segDesc = new AqSegmentDescriptor[nbrSegments];
+    # segDesc = [structs.AqSegmentDescriptor] * nbr.Segments.value
+    
+    readPar.dataType = ctypes.c_long(0)
+    readPar.readMode = ctypes.c_long(1)
+    readPar.firstSegment = ctypes.c_long(0)
+    readPar.nbrSegments = ctypes.c_long(nbrSegments)
+    readPar.firstSampleInSeg = ctypes.c_long(0)
+    readPar.nbrSamplesInSeg = nbrSamples
+    readPar.segmentOffset = nbrSamples
+    readPar.dataArraySize = ctypes.c_long((nbrSamples.value * tbNextSegmentPad.value) * (nbrSegments.value + 1) * (readPar.dataType +1))
+    readPar.segDescArraySize = ctypes.c_long((nbrSegments.value * ctypes.sizeof(structs.AqSegmentDescriptor)))
+    readPar.flags = ctypes.c_long(0)
+    readPar.reserved = ctypes.c_long(0)
+    readPar.reserved2 = ctypes.c_double(0)
+    readPar.reserved3 = ctypes.c_double(0)
+    
+    # ViInt8* adcArray = new ViInt8[(nbrSamples + tbNextSegmentPad)*(nbrSegments + 1)];
+    # adcArray = [visa.ViInt8] * ((nbrSamples.value + tbNextSegmentPad.value) * (nbrSegments.value + 1))
+    status = AqDrv4.AcqrsD1_readData(Instrument[0], channel, ctypes.byref(readPar), adcArray, ctypes.byref(dataDesc), segDesc)
+    
+    #Write Data to a file
+    now = str(datetime.now())
+    filename = ("/home/darkdaq1/Acqiris %s.data" % now).replace(" ", "_")
+    file = open(filename, 'w')
+    file.write("# Acqiris Waveforms")
+    file.write("# Channel: %s" % channel)
+    file.write("# Samples acquired: %s" % dataDesc.returnedSamplesPerSeg.value)
+    file.write("# Segments acquired: %s" % dataDesc.returnedSegments.value)
 
+        
 def Close():
     status = AqDrv4.Acqrs_closeAll()
     
-if __name__ == __main__:
+if __name__ == '__main__':
 
     print "Starting Acquisition"
     
